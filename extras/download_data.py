d6139107d6c26c0b2818d536454fafa1436b3fa5
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-04-04 19:42:14 -0600

    add new dataset

---
+++ extras/download_data.py
@@ -0,0 +1,22 @@
+import urllib.request
+import os
+import pathlib
+
+DATASET_DIR = pathlib.Path('../datasets')
+weather_station_dataset = DATASET_DIR / 'weather_station_data.parquet'
+nc_dataset = DATASET_DIR / 'berkeley_earth_dataset.nc'
+
+datasets = [
+    (weather_station_dataset,
+    'https://s3.amazonaws.com/sea-datasets/weather_station_data.parquet'),
+    (nc_dataset,
+    'https://s3.amazonaws.com/sea-datasets/berkeley_earth_dataset.nc')
+    ]
+
+for dataset in datasets:
+    if not DATASET_DIR.exists():
+        os.mkdir(DATASET_DIR)
+    if not dataset[0].exists():
+        print('Downloading', dataset[0])
+        f = urllib.request.urlretrieve(dataset[1], dataset[0])
+        print('Finished downloading......')
